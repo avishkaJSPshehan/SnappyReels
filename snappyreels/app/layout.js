573ded3dd5685ca25f43a1ffd7553ba43ab20daa
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-02-13 00:43:14 +0530

    Add fonts :)

--- snappyreels/app/layout.js
+++ snappyreels/app/layout.js
@@ -1,17 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "./provider";
+import { Lexend } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Lexend({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SnappyReels",
@@ -22,9 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={outfit.className}>
           <Provider>{children}</Provider>
         </body>
       </html>
