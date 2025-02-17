649bcd648da1510507bfdc1eaf15df3e4cf1285f
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-02-17 22:25:00 +0530

    UI Library not working 💢

--- snappyreels/app/dashboard/_components/Header.jsx
+++ snappyreels/app/dashboard/_components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+import {Button} from '@/components/ui/button'
 
 function Header() {
   return (
-    <div>
-      
+    <div className='p-3 px-5'>
+      <div className='flex gap-3 items-center'>
+        <Image src={'/logo.png'} width={30} height={30}/>
+        <h2 className='font-bold text-xl'>SnappyReels</h2>
+      </div>
+
+      <div>
+        <Button></Button>
+        <UserButton/>
+      </div>
+
+
     </div>
   )
 }
